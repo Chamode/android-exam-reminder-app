c800a5c654efcb0a161d99ecb6d5b579cb2ea114
Chamode <chamodeanjana@gmail.com>
2018-06-30 02:56:43 +0800

    first commit

---
+++ app/src/main/java/mobileapptaylors/example/MAD_assignment1/Announcement.java
@@ -0,0 +1,43 @@
+package mobileapptaylors.example.MAD_assignment1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class Announcement extends AppCompatActivity {
+
+    private ArrayList<AnnouncementModel> arrayList;
+    private AnnouncementAdapter adapter;
+    private ListView announcementList;
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), Home.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(mobileapptaylors.example.MAD_assignment1.R.layout.activity_announcement);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        announcementList = (ListView) findViewById(mobileapptaylors.example.MAD_assignment1.R.id.announcementList);
+        arrayList = new ArrayList<>();
+        arrayList.add(new AnnouncementModel("Before Examination", "For Non-MCQ examination", mobileapptaylors.example.MAD_assignment1.R.raw.no_mcq));
+        arrayList.add(new AnnouncementModel("Before Examination", "For MCQ examination", mobileapptaylors.example.MAD_assignment1.R.raw.no_mcq));
+        arrayList.add(new AnnouncementModel("During Examination", "Reminder (20mins before end)", mobileapptaylors.example.MAD_assignment1.R.raw.reminder));
+        arrayList.add(new AnnouncementModel("Complete", "Upon Completion!", mobileapptaylors.example.MAD_assignment1.R.raw.time_up));
+
+        adapter = new AnnouncementAdapter(this, mobileapptaylors.example.MAD_assignment1.R.layout.custom_announcement, arrayList);
+        announcementList.setAdapter(adapter);
+
+
+    }
+}
